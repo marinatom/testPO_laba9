79dab7f8da40566666f8d3299e215edbd9c1deed
marinatom <91328159+marinatom@users.noreply.github.com>
2025-12-29 18:02:48 +0300

    Update test_negative.py

--- lab4/test_negative.py
+++ lab4/test_negative.py
@@ -1,22 +1,31 @@
 import time
+import os
 
 class TestNegative:
-    URL = "file:///C:/Users/Серёжа\Desktop\lab4/contact_form.html"
+    # Используем относительный путь
+    URL = "file://" + os.path.join(os.path.dirname(os.path.abspath(__file__)), "contact_form.html")
+    
     def test_empty(self, driver):
         from contact_page import ContactPage
+        
+        # Отладка
+        print(f"Используем URL: {self.URL}")
+        
         page = ContactPage(driver, self.URL)
         page.open_contact_form()
-
+        
         test_data = {
             "full_name": "Иванов Иван Иванович",
-            "email": "", 
+            "email": "",
             "topic": "general",
             "message": "",
             "agreement": True
         }
+        
         page.fill_all(test_data)
         time.sleep(0.5)
         page.submit_form()
         time.sleep(1)
-
+        
         assert page.is_element_visible(page.EMAIL_ERROR)
+        assert not page.is_element_visible(page.SUCCESS_MESSAGE)
