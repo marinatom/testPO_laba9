7583451fb18caa3242a168cc6940c31cc21c8f95
marinatom <91328159+marinatom@users.noreply.github.com>
2025-12-29 17:58:34 +0300

    Update contact_page.py

--- lab4/contact_page.py
+++ lab4/contact_page.py
@@ -3,12 +3,15 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import Select
 
 class ContactPage(BasePage):
+    # Проверьте эти локаторы!
     FULL_NAME_INPUT = (By.ID, "fullName")
     EMAIL_INPUT = (By.ID, "email")
     TOPIC_SELECT = (By.ID, "topic")
     MESSAGE_TEXTAREA = (By.ID, "message")
     AGREEMENT_CHECKBOX = (By.ID, "agreement")
     SUBMIT_BUTTON = (By.CSS_SELECTOR, "button[type='submit']")
+    
+    # Локаторы сообщений об ошибках
     FULL_NAME_ERROR = (By.ID, "fullNameError")
     EMAIL_ERROR = (By.ID, "emailError")
     TOPIC_ERROR = (By.ID, "topicError")
@@ -20,44 +23,65 @@ class ContactPage(BasePage):
         super().__init__(driver, url)
     
     def open_contact_form(self):
-        self.open()
+        self.driver.get(self.url)  # Просто открываем URL
     
     def enter_full_name(self, full_name):
-        element = self.find_clickable_element(self.FULL_NAME_INPUT)
-        element.clear()
-        element.send_keys(full_name)
+        element = self.find_element(self.FULL_NAME_INPUT, timeout=10)
+        if element:
+            element.clear()
+            element.send_keys(full_name)
+        else:
+            print(f"Элемент {self.FULL_NAME_INPUT} не найден!")
     
     def enter_email(self, email):
-        element = self.find_clickable_element(self.EMAIL_INPUT)
-        element.clear()
-        element.send_keys(email)
+        element = self.find_element(self.EMAIL_INPUT, timeout=10)
+        if element:
+            element.clear()
+            element.send_keys(email)
+        else:
+            print(f"Элемент {self.EMAIL_INPUT} не найден!")
     
     def select_topic(self, topic_key):
-        element = self.find_clickable_element(self.TOPIC_SELECT)
-        select = Select(element)
-        select.select_by_value(topic_key)
+        element = self.find_element(self.TOPIC_SELECT, timeout=10)
+        if element:
+            select = Select(element)
+            select.select_by_value(topic_key)
+        else:
+            print(f"Элемент {self.TOPIC_SELECT} не найден!")
     
     def enter_message(self, message):
-        element = self.find_clickable_element(self.MESSAGE_TEXTAREA)
-        element.clear()
-        if message:
-            element.send_keys(message)
+        element = self.find_element(self.MESSAGE_TEXTAREA, timeout=10)
+        if element:
+            element.clear()
+            if message:
+                element.send_keys(message)
+        else:
+            print(f"Элемент {self.MESSAGE_TEXTAREA} не найден!")
     
     def check_agreement(self):
-        element = self.find_clickable_element(self.AGREEMENT_CHECKBOX)
-        if not element.is_selected():
+        element = self.find_element(self.AGREEMENT_CHECKBOX, timeout=10)
+        if element and not element.is_selected():
             element.click()
+        elif not element:
+            print(f"Элемент {self.AGREEMENT_CHECKBOX} не найден!")
     
     def uncheck_agreement(self):
-        element = self.find_clickable_element(self.AGREEMENT_CHECKBOX)
-        if element.is_selected():
+        element = self.find_element(self.AGREEMENT_CHECKBOX, timeout=10)
+        if element and element.is_selected():
             element.click()
+        elif not element:
+            print(f"Элемент {self.AGREEMENT_CHECKBOX} не найден!")
     
     def submit_form(self):
-        element = self.find_clickable_element(self.SUBMIT_BUTTON)
-        element.click()
+        element = self.find_clickable_element(self.SUBMIT_BUTTON, timeout=10)
+        if element:
+            element.click()
+        else:
+            print(f"Элемент {self.SUBMIT_BUTTON} не найден!")
     
     def fill_all(self, data):
+        """Заполнить все поля формы"""
+        print(f"Заполняем форму данными: {data}")
         self.enter_full_name(data.get("full_name", ""))
         self.enter_email(data.get("email", ""))
         self.select_topic(data.get("topic", "general"))
